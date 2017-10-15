93dac11dc1010741e1207b1393bb6ac17b12bfb8
Sanaa Fidahussain <sanfidahussain@gmail.com>
2017-10-15 19:03:39 +0000

    change user

--- models/user.js
+++ models/user.js
@@ -1,9 +1,6 @@
 var mongoose   = require("mongoose"),
     passportLocalMongoose = require("passport-local-mongoose");
     
-// Connect to db
-mongoose.connect("mongodb://localhost/yelp_camp_v12", {useMongoClient: true});
-
 // SCHEMA SETUP
 var UserSchema = new mongoose.Schema({
     username: String,
